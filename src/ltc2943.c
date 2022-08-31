6cdfe8c20e51266982b79dfef25d025ce0744e7f
Aron Boeroeczky <ab@aradas.lt>
2022-08-31 20:59:58 +0300

    check alert pending

--- src/ltc2943.c
+++ src/ltc2943.c
@@ -1,18 +1,25 @@
 #include "ltc2943.h"
 #include "i2c_driver.h"
 
-bool ltc2943_getMode(controlFields* controls)
+bool ltc2943_getMode(controlFields *controls)
 {
-	if(!i2cInitialize())
+	if (!i2cInitialize())
 	{
 		return false;
 	}
 
-	i2cWrite(LTC2943_ADDRESS << 1, CONTROL_REG, 1);
-	uint8_t controlRegisterContent = 0x00;
-	i2cRead(LTC2943_ADDRESS << 1, &controlRegisterContent, 1);
+	if (!i2cWrite(LTC2943_ADDRESS << 1, CONTROL_REG, 1))
+	{
+		return false;
+	}
 
-	
+	uint8_t controlRegisterContent = 0x00;
+
+	if (!i2cRead(LTC2943_ADDRESS << 1, &controlRegisterContent, 1))
+	{
+		return false;
+	}
+
 	controls->mode = controlRegisterContent & ADCMODE_MASK;
 	controls->prescaler = controlRegisterContent & PRESCALER_MASK;
 	controls->allcPinMode = controlRegisterContent & ALLCPIN_MASK;
@@ -23,7 +30,7 @@ bool ltc2943_getMode(controlFields* controls)
 
 bool ltc2943_setMode(controlFields controls)
 {
-	if(!i2cInitialize())
+	if (!i2cInitialize())
 	{
 		return false;
 	}
@@ -31,23 +38,61 @@ bool ltc2943_setMode(controlFields controls)
 	uint8_t controlData[2];
 	controlData[0] = CONTROL_REG;
 	controlData[1] = controls.mode | controls.prescaler | controls.allcPinMode | controls.shutDown;
-	
+
 	return i2cWrite(LTC2943_ADDRESS << 1, controlData, sizeof(controlData));
 }
 
 bool ltc2943_checkVoltageAlertPending(bool *alertPending)
 {
+	if (!i2cInitialize())
+	{
+		return false;
+	}
+
+	if (!i2cWrite(LTC2943_ADDRESS << 1, STATUS_REG, 1))
+	{
+		return false;
+	}
+
+	uint8_t statusRegisterContent = 0x00;
+
+	if (!i2cRead(LTC2943_ADDRESS << 1, &statusRegisterContent, 1))
+	{
+		return false;
+	}
+
+	*alertPending = statusRegisterContent >> 1 & 1;
+
 	return true;
 }
 
 bool ltc2943_checkTemperatureAlertPending(bool *alertPending)
 {
+	if (!i2cInitialize())
+	{
+		return false;
+	}
+
+	if (!i2cWrite(LTC2943_ADDRESS << 1, STATUS_REG, 1))
+	{
+		return false;
+	}
+
+	uint8_t statusRegisterContent = 0x00;
+
+	if (!i2cRead(LTC2943_ADDRESS << 1, &statusRegisterContent, 1))
+	{
+		return false;
+	}
+
+	*alertPending = statusRegisterContent >> 4 & 1;
+
 	return true;
 }
 
 bool i2cInitialize()
 {
-	if(i2cIsInitialized())
+	if (i2cIsInitialized())
 	{
 		return true;
 	}
